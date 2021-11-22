184b69a02888a37c7203746560cec7eeaa7c57e2
Marcia Castro <marcas4@gmail.com>
2021-11-22 17:50:33 -0500

    Se generaron las rutas y la navegación en la aplicación, se creó el header donde se tiene la navegación a cada una de las páginas y se creo la página de 404 no found por ultimo se generó un servicio llamado products.service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
   {
     path: 'home',
     component: HomeComponent
@@ -21,6 +27,10 @@ const routes: Routes = [
   {
     path: 'demo',
     component: DemoComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
 import { SwiperModule } from 'swiper/angular';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
  
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { SwiperModule } from 'swiper/angular';
     HeaderComponent,
     FooterComponent,
     BannerComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
