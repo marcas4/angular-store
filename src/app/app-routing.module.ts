042de5feaf9bea3c252050854e212cdbb95d7ff7
Marcia Castro <marcas4@gmail.com>
2021-11-27 07:51:17 -0500

    Se genera el archivo admin.guard y se llama desde el app-routing para el contact, pero en este momento está retornando por defecto true

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AdminGuard } from './admin.guard';
 import { DemoComponent } from './demo/demo.component';
 import { LayoutComponent } from './layout/layout.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -24,6 +25,7 @@ const routes: Routes = [
       },
       {
         path: 'contact',
+        canActivate: [AdminGuard],
         loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
       },
     ]
