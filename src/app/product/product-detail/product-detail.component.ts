85a2d47eefeba56a4dd129d059c9731b0e178092
Marcia Castro <marcas4@gmail.com>
2021-12-02 16:31:13 -0500

    Se creo el método para crear el producto desde angular y se imnplementó en un botón en el product-detail component

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -33,4 +33,17 @@ export class ProductDetailComponent implements OnInit {
     });
   }
 
+  createProduct() {
+    const newProduct: Product = {
+      id: '222',
+      title: 'nuevo desde angular',
+      image: 'assets/images/banner-1.jpg',
+      price: 30000,
+      description: 'nuevo producto'
+    };
+    this.productService.createProduct(newProduct).subscribe(product => {
+      console.log(product);
+    });
+  }
+
 }
